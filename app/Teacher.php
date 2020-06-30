6873b6f26804f622380215fdff7e391571c0979f
Deadman <tharindu.2015188@iit.ac.lk>
2020-06-30 09:29:24 +0530

    Assesment

---
+++ app/Teacher.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Teacher extends Model
+{
+    protected $fillable = ['teacher_name'];
+
+    public function classRoom()
+    {
+        return $this->hasMany('App\ClassRoom');
+    }
+}
+ 
